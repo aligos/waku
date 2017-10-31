b033e468b0f6481e9540f511a5a0c142d7b854e2
aligos <artasikindonesia@gmail.com>
2017-10-31 20:33:34 +0700

    Add flask login

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 from config import app_config
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 
 def create_app(config_name):
@@ -16,5 +18,9 @@ def create_app(config_name):
     @app.route('/')
     def hello_world():
         return 'Hello, World!'
+
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view = "auth.login"
 
     return app
